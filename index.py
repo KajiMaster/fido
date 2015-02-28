bda969b64163b09454e9f7c02ee8cec2d74e2b4b
Josh Kaderlan <jkaderlan@yahoo.com>
2015-02-27 17:05:03 -0800

    work in progress

--- index.py
+++ index.py
@@ -1,5 +1,7 @@
+from collections import Counter
 from urlparse import urlparse
 
+from bs4 import BeautifulSoup
 from flask import Flask, render_template
 from flask import request as rq
 import requests
@@ -24,6 +26,7 @@ def fetch():
         return render_template('index.html', error='No URL specified!')
     # validate the URL
     pr = urlparse(url)
+    # this check also catches relative urls
     if pr.scheme not in ('http', 'https'):
         return render_template('index.html', 
                 error='Only HTTP- or HTTPS-based URLs allowed!')
@@ -34,7 +37,29 @@ def fetch():
     except Exception as e:
         return render_template('index.html', error='There was a problem with \
                 your request. Please doublecheck your URL.')
-    return render_template('index.html', error=error, url=url)
+    # requests automatically follows redirects
+    # so the final status code should be a 200
+    if r.status_code != requests.codes.ok:
+        return render_template('index.html',
+                error='Got an error from the remote server!')
+    # we only try to parse HTML
+    # i'm not overjoyed with this as the only check to make sure the
+    # payload is in fact HTML, but i'm not sure what other options there are
+    if r.headers['content-type'] != 'text/html':
+        return render_template('index.html',
+                error='The server returned something other than HTML!')
+    source_html = r.text
+    try:
+        soup = BeautifulSoup(source_html)
+    except Exception as e:
+        return render_template('index.html',
+                error='There was a problem with the target HTML!')
+    tags = [tag.name for tag in soup.findAll()]
+    tag_dict = dict(Counter(tags))
+    return render_template('index.html', 
+            error=error, 
+            source_html=source_html,
+            tag_dict=tag_dict)
 
 
 
