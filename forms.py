5b4bc82d21b1b4d557715239030ed5c4b21975d8
Josh Kaderlan <jkaderlan@yahoo.com>
2015-03-01 18:31:55 -0800

    switch to flask-wtf for form

--- forms.py
+++ forms.py
@@ -1,8 +1,24 @@
+from urlparse import urlparse
+
 from flask_wtf import Form
+#from flask.ext.wtf import Form
 from wtforms import StringField
-from wtforms.validators import URL
+from wtforms.validators import InputRequired, URL, ValidationError
 
 class URLForm(Form):
-    url = StringField(i"URL", validators=[URL()])
+    url = StringField('URL', validators=[InputRequired()])
+
+    def validate_url(form, field):
+        try:
+            pr = urlparse(field.data)
+        except Exception as e:
+            raise ValidationError(e)
+        if pr.scheme not in ('http', 'https'):
+            raise ValidationError('Only HTTP- or HTTPS-based urls allowed!')
+        if not pr.netloc:
+            raise ValidationError('Relative URLs not allowed!')
+        if '.' not in pr.netloc:
+            raise ValidationError('Must specify a TLD!')
+
 
 
--- index.py
+++ index.py
@@ -4,11 +4,14 @@ from urlparse import urlparse
 from bs4 import BeautifulSoup
 from flask import Flask, render_template, redirect
 from flask import request as rq
+from flask_wtf.csrf import CsrfProtect
 import requests
 
-app = Flask(__name__)
-app.debug = True
+import forms
 
+app = Flask(__name__)
+#csrf = CsrfProtect(app)
+app.debug = True
 
 # first view shows the URL entry box
 @app.route('/')
@@ -16,28 +19,39 @@ def index():
     return render_template('test.html')
 
 #the form on the index page hits this url
+#@csrf.exempt
 @app.route('/fetch', methods=['GET', 'POST'])
 def fetch():
-    if rq.method == 'POST':
-        #error = None
+    tag_dict = None
+    form = forms.URLForm(csrf_enabled=False)
+    #form = forms.URLForm(rq.POST)
+    #if rq.method == 'POST':
+    if form.validate_on_submit():
+        #form = forms.URLForm(rq.form, csrf_enabled=False)
+        #if not form.validate():
+        #    return render_template('index.html', form=form)
+        error = None
         #url = None
-        url = rq.form.get('url')
+        #url = rq.form.get('url')
+        url = form.url.data
+        print url
         # make sure the user actually passed us a URL
-        if not url:
-            return render_template('index.html', error='No URL specified!')
+        #if not url:
+        #    return render_template('index.html', error='No URL specified!')
         # validate the URL
-        pr = urlparse(url)
-        # this check also catches relative urls
-        if pr.scheme not in ('http', 'https'):
-            return render_template('index.html', 
-                    error='Only HTTP- or HTTPS-based URLs allowed!')
+        #pr = urlparse(url)
+        # this check also catches relative urls, which we don't handle
+        #if pr.scheme not in ('http', 'https'):
+        #    form.url.errors = ['Only HTTP- or HTTPS-based urls allowed!']
+        #    return render_template('index.html', form=form)
+                    #error='Only HTTP- or HTTPS-based URLs allowed!')
         #if not pr.netloc:
         #    return render_template('index.html', error='Relative URLs not allowed!')
         try:
             r = requests.get(url)
         except Exception as e:
             return render_template('index.html', 
-            error='There was a problem with your request. Please doublecheck your URL.')
+                    error='There was a problem with your request. Please doublecheck your URL.')
         # requests automatically follows redirects
         # so the final status code should be a 200
         if r.status_code != requests.codes.ok:
@@ -46,7 +60,8 @@ def fetch():
         # we only try to parse HTML
         # i'm not overjoyed with this as the only check to make sure the
         # payload is in fact HTML, but i'm not sure what other options there are
-        if r.headers['content-type'] != 'text/html':
+        if not r.headers['content-type'].startswith('text/html'):
+            print r.headers['content-type']
             return render_template('index.html',
                     error='The server returned something other than HTML!')
         source_html = r.text
@@ -59,10 +74,12 @@ def fetch():
         tag_dict = dict(Counter(tags))
         return render_template('index.html', 
                 error=error, 
-                source_html=source_html,
+                form=form,
+                #source_html=source_html,
+                source_html=soup.prettify(),
                 tag_dict=tag_dict)
-
-    return render_template('index.html')
+    #form = forms.URLForm(csrf_enabled=False)
+    return render_template('index.html', form=form, tag_dict=tag_dict)
 
 
 
